c6cb5f87f0f3f54bdd4ae15d848ccd79779b527c
MinseopChoi <minsubb13@gmail.com>
2024-06-08 19:18:31 +0900

    main port번호 수정

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ async def welcome() -> dict:
 app.include_router(comment_router)  # added
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
