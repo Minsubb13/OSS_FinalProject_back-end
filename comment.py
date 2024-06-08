9cef65476aa46f22f3e88d59d1d990a78254f88e
MinseopChoi <minsubb13@gmail.com>
2024-06-09 02:23:14 +0900

    첫번째 완성

--- comment.py
+++ comment.py
@@ -7,6 +7,7 @@ comment_list = []
 
 @comment_router.post("/comment")
 async def add_comment(comment: Comment) -> dict:
+    comment.id = len(comment_list) + 1
     comment_list.append(comment)
     return {
         "msg": "visiter's comments added successfully",
@@ -26,19 +27,10 @@ async def get_single_comment(comment_id: str = Path(..., title="the ID of the co
             return {"visiter's comments": comment}
     return {"msg": "visiter's comments with supplied ID doesn't exist"}
 
-# @comment_router.delete("/comment/{comment_id}")
-# async def delete_comment(comment_id: str = Path(..., title="the ID of the comment to delete")) -> dict:
-#         for id, comment in comment_list:
-#             if comment.id == comment_id:
-#                 del comment_list[id]
-#                 return {"msg": f"visiter's comments {comment_id} deleted successfully"}
-#         return {"msg": "visiter's comments with supplied ID doesn't exist"}
-
-
 @comment_router.delete("/comment/{comment_id}")
-async def delete_comment(comment_id: str):
-    for i, comment in enumerate(comment_list):
+async def delete_comment(comment_id: int = Path(..., title="the ID of the comment to delete")) -> dict:
+    for index, comment in enumerate(comment_list):
         if comment.id == comment_id:
-            comment_list.pop(i)
-            return {"message": "Comment deleted successfully"}
+            del comment_list[index]
+            return {"message": f"Comment {comment_id} deleted successfully"}
     return {"message": "Comment not found"}
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
 class Comment(BaseModel):
-    id: str
+    id: int
+    name : str
     comment: str
